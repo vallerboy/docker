5137ca514b4804b1468723ac011b334e6d7df2f5
Oskix <oskar@boxly.pl>
2019-02-23 21:06:03 +0100

    FirstCommit

---
+++ src/main/java/pl/oskarpolak/docker/MainController.java
@@ -0,0 +1,16 @@
+package pl.oskarpolak.docker;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld()  {
+        return "Hello Docker on AWS!";
+    }
+}
+
